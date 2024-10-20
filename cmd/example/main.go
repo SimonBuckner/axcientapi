fe1ad364e624b804ff62b164a2bbf1bb3ec1d0c4
Simon Buckner <simonbuckner@hotmail.com>
2024-10-20 10:07:14 +0100

    100% coverage of the GET API.
    Todo:
    - Readme on library and example
    - Dealing with Panics
    - Better error messages

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	baseUrl := os.Getenv("BASE_URL")
 	apiKey := os.Getenv("API_KEY")
-	// vaultId := apihelper.StringtoI64(os.Getenv("VAULT_ID"))
+	vaultId := apihelper.StringtoI64(os.Getenv("VAULT_ID"))
 	clientId := apihelper.StringtoI64(os.Getenv("CLIENT_ID"))
 	deviceId := apihelper.StringtoI64(os.Getenv("DEVICE_ID"))
 	jobId := apihelper.StringtoI64(os.Getenv("JOB_ID"))
@@ -31,98 +31,97 @@ func main() {
 	axcient := axcient.NewAxcientApi(baseUrl, true, true)
 	axcient.Authenticate(apiKey)
 
-	// // -------------------------------------------------------------------------
-	// //   Endpoints starting /vault
-	// // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	//   Endpoints starting /vault
+	// -------------------------------------------------------------------------
 
-	// printBanner("GET:/vault")
-	// allVaults, err := axcient.GetVaults().SetIncludeDevices(true).GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(allVaults)
+	printBanner("GET:/vault")
+	allVaults, err := axcient.GetVaults().SetIncludeDevices(true).GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(allVaults)
 
-	// printBanner("GET:/vault/{vault_id}")
-	// singleVault, err := axcient.GetVaults().
-	// 	SelectByVaultId(vaultId).
-	// 	GetSingle()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(singleVault)
+	printBanner("GET:/vault/{vault_id}")
+	singleVault, err := axcient.GetVaults().
+		SelectByVaultId(vaultId).
+		GetSingle()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(singleVault)
 
-	// printBanner("GET:/vault/{vault_id}/threshold/connectivity")
-	// singleVaultThreshold, err := axcient.GetVaultVaultThresholdConnectivity(vaultId).
-	// 	GetSingle()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(singleVaultThreshold)
+	printBanner("GET:/vault/{vault_id}/threshold/connectivity")
+	singleVaultThreshold, err := axcient.GetVaultVaultThresholdConnectivity(vaultId).
+		GetSingle()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(singleVaultThreshold)
 
-	// // -------------------------------------------------------------------------
-	// //   Endpoints starting /device
-	// // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	//   Endpoints starting /device
+	// -------------------------------------------------------------------------
 
-	// printBanner("GET:/device")
-	// devices, err := axcient.GetDevices().GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(devices)
+	printBanner("GET:/device")
+	devices, err := axcient.GetDevices().GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(devices)
 
-	// printBanner("GET:/device/{device_id}")
-	// device, err := axcient.GetDevices().SelectByDeviceId(deviceId).GetSingle()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(device)
+	printBanner("GET:/device/{device_id}")
+	device, err := axcient.GetDevices().SelectByDeviceId(deviceId).GetSingle()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(device)
 
-	// printBanner("GET:/device/{device_id}/autoverify")
-	// autoverify, err := axcient.GetDeviceAutoverifyDetails().SelectByDeviceId(deviceId).GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(autoverify)
+	printBanner("GET:/device/{device_id}/autoverify")
+	autoverify, err := axcient.GetDeviceAutoverifyDetails().SelectByDeviceId(deviceId).GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(autoverify)
 
-	// printBanner("GET:/device/{device_id}/restore_point")
-	// restorePoints, err := axcient.GetDeviceRestorePoints().SelectByDeviceId(deviceId).GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(restorePoints)
+	printBanner("GET:/device/{device_id}/restore_point")
+	restorePoints, err := axcient.GetDeviceRestorePoints().SelectByDeviceId(deviceId).GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(restorePoints)
 
-	// // -------------------------------------------------------------------------
-	// //   Endpoints starting /client
-	// // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	//   Endpoints starting /client
+	// -------------------------------------------------------------------------
 
-	// printBanner("GET:/client")
-	// allClients, err := axcient.GetClients().SetIncludeAppliances(true).GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(allClients)
+	printBanner("GET:/client")
+	allClients, err := axcient.GetClients().SetIncludeAppliances(true).GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(allClients)
 
-	// printBanner("GET:/client/{client_id}")
-	// singleClient, err := axcient.GetClients().
-	// 	SetIncludeAppliances(true).
-	// 	SelectByClientId(clientId).
-	// 	GetSingle()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(singleClient)
+	printBanner("GET:/client/{client_id}")
+	singleClient, err := axcient.GetClients().
+		SetIncludeAppliances(true).
+		SelectByClientId(clientId).
+		GetSingle()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(singleClient)
 
-	// printBanner("GET:/client/{client_id}/device")
-	// clientDevices, err := axcient.GetClientDevices().
-	// 	SelectByClientId(clientId).
-	// 	SetDirect2Cloud(true).
-	// 	GetAll()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(clientDevices)
+	printBanner("GET:/client/{client_id}/device")
+	clientDevices, err := axcient.GetClientDevices().
+		SelectByClientId(clientId).
+		SetDirect2Cloud(true).
+		GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(clientDevices)
 
-	// printBanner("GET:/client/{client_id}/device/{device_id}/job")
 	printBanner("GET:/client/{client_id}/device/{device_id}/job")
 	deviceJobs, err := axcient.GetJobs().
 		SelectByClientId(clientId).
@@ -143,35 +142,43 @@ func main() {
 		panic(err)
 	}
 	dump.Print(deviceJob)
-	//
 
-	// printBanner("GET:/client/{client_id}/device/{device_id}/job/{job_id}/history")
+	printBanner("GET:/client/{client_id}/device/{device_id}/job/{job_id}/history")
+	jobHistory, err := axcient.GetJobHistory().
+		SelectByClientId(clientId).
+		SelectByDeviceId(deviceId).
+		SelectByJobId(jobId).
+		GetAll()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(jobHistory)
 
-	// // -------------------------------------------------------------------------
-	// //   Endpoints starting /organization
-	// // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	//   Endpoints starting /organization
+	// -------------------------------------------------------------------------
 
-	// printBanner("Get Organisation Details")
-	// org, err := axcient.GetOrganisation().GetSingle()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(org)
+	printBanner("Get Organisation Details")
+	org, err := axcient.GetOrganisation().GetSingle()
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(org)
 
-	// // -------------------------------------------------------------------------
-	// //   Endpoints starting /organization
-	// // -------------------------------------------------------------------------
+	// -------------------------------------------------------------------------
+	//   Endpoints starting /organization
+	// -------------------------------------------------------------------------
 
-	// printBanner("POST:/client/{client_id}/vault/{vault_id}/d2c_agent")
-	// d2c, err := axcient.GetD2CAgent().
-	// 	SelectByClientId(clientId).
-	// 	SelectByVaultId(vaultId).
-	// 	GetSingle()
+	printBanner("POST:/client/{client_id}/vault/{vault_id}/d2c_agent")
+	d2c, err := axcient.GetD2CAgent().
+		SelectByClientId(clientId).
+		SelectByVaultId(vaultId).
+		GetSingle()
 
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// dump.Print(d2c)
+	if err != nil {
+		panic(err)
+	}
+	dump.Print(d2c)
 }
 
 func printBanner(message string) {
@@ -181,13 +188,3 @@ func printBanner(message string) {
 	fmt.Println("-------------------------------------------------------------")
 	fmt.Println()
 }
-
-// func printSubBanner(message string) {
-// 	fmt.Println("    " + message)
-// 	fmt.Println("    ---------------------------------------------------------")
-// 	fmt.Println()
-// }
-
-// func printStatus(status string) {
-// 	fmt.Println("    Status: " + status)
-// }
