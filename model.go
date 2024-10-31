c6129fa95b1abd7cbb353b46d20c66c6143e4b75
Simon Buckner <simonbuckner@hotmail.com>
2024-10-31 08:00:55 +0000

    Refactor timestamps from string to time.Time

--- model.go
+++ model.go
@@ -1,5 +1,10 @@
 package axcientapi
 
+import (
+	"strings"
+	"time"
+)
+
 // Source schemas can be found here :https://developer.axcient.com/x360recover/
 
 type OrgLevelAppliance struct {
@@ -14,7 +19,7 @@ type OrgLevelAppliance struct {
 	Model              *OrgLevelServerModel           `json:"model"`
 	Package            *OrgLevelPackage               `json:"package"`
 	ServiceType        string                         `json:"service_type"`
-	LastTunnelUp       string                         `json:"last_tunnel_up"`
+	LastTunnelUp       time.Time                      `json:"last_tunnel_up"`
 	TunnelStatus       string                         `json:"tunnel_status"`
 	StorageDetails     *StorageDetail                 `json:"storage_details"`
 	Devices            []OrgLevelShortApplianceDevice `json:"devices"`
@@ -44,7 +49,7 @@ type OrgLevelVault struct {
 	Type               string                `json:"type"`
 	Model              *OrgLevelServerModel  `json:"model"`
 	ServiceType        string                `json:"service_type"`
-	LastTunnelUp       string                `json:"last_tunnel_up"`
+	LastTunnelUp       time.Time             `json:"last_tunnel_up"`
 	TunnelStatus       string                `json:"tunnel_status"`
 	StorageDetails     *StorageDetail        `json:"storage_details"`
 	Devices            []OrgLevelShortDevice `json:"devices"`
@@ -53,14 +58,14 @@ type OrgLevelVault struct {
 	VaultThresholds    *VaultThresholds      `json:"vault_thresholds"`
 	HealthStatus       string                `json:"health_status"`
 	HealthStatusReason string                `json:"health_status_reason"`
-	CreationTimestamp  string                `json:"creation_timestamp"`
+	CreationTimestamp  time.Time             `json:"creation_timestamp"`
 }
 
 type OrgLevelShortVault struct {
-	Id            int64  `json:"id"`
-	Type          string `json:"type"`
-	LatestVaultRp string `json:"latest_vault_rp"`
-	DeviceUsage   int64  `json:"device_usage"`
+	Id            int64     `json:"id"`
+	Type          string    `json:"type"`
+	LatestVaultRp time.Time `json:"latest_vault_rp"`
+	DeviceUsage   int64     `json:"device_usage"`
 }
 
 type OrgLevelDevice struct {
@@ -82,9 +87,9 @@ type OrgLevelDevice struct {
 	CloudUsage              int64                            `json:"cloud_usage"`
 	VaultUsage              int64                            `json:"vault_usage"`
 	Jobs                    []OrgLevelShortJob               `json:"jobs"`
-	LatestCloudRp           string                           `json:"latest_cloud_rp"`
-	LatestLocalRp           string                           `json:"latest_local_rp"`
-	LatestVaultRp           string                           `json:"latest_vault_rp"`
+	LatestCloudRp           time.Time                        `json:"latest_cloud_rp"`
+	LatestLocalRp           time.Time                        `json:"latest_local_rp"`
+	LatestVaultRp           time.Time                        `json:"latest_vault_rp"`
 	Direct2Cloud            bool                             `json:"d2c"`
 	AsioEndpointId          string                           `json:"asio_endpoint _id"`
 	AgentVersion            string                           `json:"agent_version"`
@@ -105,13 +110,13 @@ type OrgLevelShortDevice struct {
 }
 
 type OrgLevelShortApplianceDevice struct {
-	IpAddress     string `json:"ip_address"`
-	Os            string `json:"os"`
-	Id            int64  `json:"id"`
-	Name          string `json:"name"`
-	Type          string `json:"type"`
-	LocalUsage    int64  `json:"local_usage"`
-	LatestLocalRp string `json:"latest_local_rp"`
+	IpAddress     string    `json:"ip_address"`
+	Os            string    `json:"os"`
+	Id            int64     `json:"id"`
+	Name          string    `json:"name"`
+	Type          string    `json:"type"`
+	LocalUsage    int64     `json:"local_usage"`
+	LatestLocalRp time.Time `json:"latest_local_rp"`
 }
 
 type OrgLevelShortJob struct {
@@ -121,12 +126,12 @@ type OrgLevelShortJob struct {
 	HealthStatus string `json:"health_status"`
 	//
 	// org_level_brc_job fields
-	LatestLocalRp *string `json:"latest_local_rp"`
-	LatestCloudRp *string `json:"latest_cloud_rp"`
+	LatestLocalRp *time.Time `json:"latest_local_rp"`
+	LatestCloudRp *time.Time `json:"latest_cloud_rp"`
 	//
 	// org_level_replibit_job fields
-	VaultId  int64   `json:"vault_id"`
-	LatestRp *string `json:"latest_rp"`
+	VaultId  int64      `json:"vault_id"`
+	LatestRp *time.Time `json:"latest_rp"`
 }
 type OrgLevelJob struct {
 	// Common fields
@@ -138,22 +143,22 @@ type OrgLevelJob struct {
 	HealthStatus string         `json:"health_status"`
 	//
 	// org_level_brc_job fields
-	JobType       *string `json:"job_type"`
-	LatestLocalRp *string `json:"latest_local_rp"`
-	LatestCloudRp *string `json:"latest_cloud_rp"`
+	JobType       *string    `json:"job_type"`
+	LatestLocalRp *time.Time `json:"latest_local_rp"`
+	LatestCloudRp *time.Time `json:"latest_cloud_rp"`
 	//
 	// org_level_replibit_job fields
 	Vault    *OrgLevelVault `json:"vault"`
 	Schedule *string        `json:"schedule"`
-	LatestRp *string        `json:"latest_rp"`
+	LatestRp *time.Time     `json:"latest_rp"`
 }
 
 type JobHistory struct {
-	Status    string `json:"status"`
-	StartTime string `json:"start_time"`
-	EndTime   string `json:"end_time"`
-	Rp        string `json:"rp"`
-	ErrorMsg  string `json:"error_msg"`
+	Status    string    `json:"status"`
+	StartTime time.Time `json:"start_time"`
+	EndTime   time.Time `json:"end_time"`
+	Rp        time.Time `json:"rp"`
+	ErrorMsg  string    `json:"error_msg"`
 }
 
 type OrgLevelServerModel struct {
@@ -208,23 +213,23 @@ type DeviceOs struct {
 }
 
 type LocalCacheDetails struct {
-	Enabled                             bool   `json:"enabled"`
-	LastSuccessfulVerificationTimestamp string `json:"last_successful_verification_timestamp"`
-	LastVerificationTimestamp           string `json:"last_verification_timestamp"`
-	Path                                string `json:"path"`
-	VerificationStatus                  string `json:"verification_status"`
+	Enabled                             bool      `json:"enabled"`
+	LastSuccessfulVerificationTimestamp time.Time `json:"last_successful_verification_timestamp"`
+	LastVerificationTimestamp           time.Time `json:"last_verification_timestamp"`
+	Path                                string    `json:"path"`
+	VerificationStatus                  string    `json:"verification_status"`
 }
 
 type OrgLevelLatestAutoverifyDetails struct {
-	Id                     string `json:"id"`
-	Timestamp              string `json:"timestamp"`
-	StartTimestamp         string `json:"start_timestamp"`
-	EndTimestamp           string `json:"end_timestamp"`
-	Rp                     string `json:"rp"`
-	Status                 string `json:"status"`
-	ScreenshotUrl          string `json:"screenshot_url"`
-	ScreenshotThumbnailUrl string `json:"screenshot_thumbnail_url"`
-	IsHealthy              bool   `json:"is_healthy"`
+	Id                     string    `json:"id"`
+	Timestamp              time.Time `json:"timestamp"`
+	StartTimestamp         time.Time `json:"start_timestamp"`
+	EndTimestamp           time.Time `json:"end_timestamp"`
+	Rp                     time.Time `json:"rp"`
+	Status                 string    `json:"status"`
+	ScreenshotUrl          string    `json:"screenshot_url"`
+	ScreenshotThumbnailUrl string    `json:"screenshot_thumbnail_url"`
+	IsHealthy              bool      `json:"is_healthy"`
 }
 
 type VaultThresholds struct {
@@ -259,9 +264,9 @@ type ThresholdObj struct {
 type OrgLevelJobResponse OrgLevelJob
 
 type HealthStatusObj struct {
-	Status    string `json:"status"`
-	Reason    string `json:"reason"`
-	Timestamp string `json:"timestamp"`
+	Status    string    `json:"status"`
+	Reason    string    `json:"reason"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 type OrgLevelOrganisation struct {
@@ -280,9 +285,9 @@ type MachineRestorePoint struct {
 }
 
 type RestorePoint struct {
-	Timestamp      string `json:"timestamp"`
-	InUse          bool   `json:"in_use"`
-	UsageInitiator string `json:"usage_initiator"`
+	Timestamp      RestorePointTimestamp `json:"timestamp"`
+	InUse          bool                  `json:"in_use"`
+	UsageInitiator string                `json:"usage_initiator"`
 }
 
 // org_level_machine_autoverify_details
@@ -293,16 +298,38 @@ type OrgLevelMachineAutoverifyDetails struct {
 }
 
 type OrgLevelAutoverifyInfo struct {
-	Id                     string `json:"id"`
-	Timestamp              string `json:"timestamp"`
-	StartTimestamp         string `json:"start_timestamp"`
-	EndTimestamp           string `json:"end_timestamp"`
-	Rp                     string `json:"rp"`
-	Status                 string `json:"status"`
-	ScreenshotUrl          string `json:"screenshot_url"`
-	ScreenshotThumbnailUrl string `json:"screenshot_thumbnail_url"`
-	IsHealthy              bool   `json:"is_healthy"`
+	Id                     string    `json:"id"`
+	Timestamp              time.Time `json:"timestamp"`
+	StartTimestamp         time.Time `json:"start_timestamp"`
+	EndTimestamp           time.Time `json:"end_timestamp"`
+	Rp                     time.Time `json:"rp"`
+	Status                 string    `json:"status"`
+	ScreenshotUrl          string    `json:"screenshot_url"`
+	ScreenshotThumbnailUrl string    `json:"screenshot_thumbnail_url"`
+	IsHealthy              bool      `json:"is_healthy"`
 }
 type ClientLevelDirect2CloudAgentToken struct {
 	TokenId string `json:"token_id"`
+}
+
+/*
+ *  Custom fields to plug holes in their API
+ *
+ */
+
+// This particular timestamp has no timezone so cannot be parsed by the default RFC
+type RestorePointTimestamp time.Time
+
+func (t *RestorePointTimestamp) UnmarshalJSON(b []byte) error {
+	value := strings.Trim(string(b), `"`) //get rid of "
+	if value == "" || value == "null" {
+		return nil
+	}
+
+	out, err := time.Parse("2006-01-02T15:04:05", value) //parse time
+	if err != nil {
+		return err
+	}
+	*t = RestorePointTimestamp(out) //set result using the pointer
+	return nil
 }
